6ec786f3bf79e871d5f8852d81236bed38f1cb48
Patrick Grupp <patrick.grupp@student.uni-tuebingen.de>
2016-07-18 19:56:33 +0200

    Intersection corrected

--- intersect.py
+++ intersect.py
@@ -14,29 +14,34 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print('Reading KEGG mapping file...')
-    kegg = pd.read_csv(args.keggmap, sep='\t')
+    kegg = pd.read_csv(args.keggmap, sep='\t', header=None)
     kegg.columns = ['gi', 'kegg']
+    kegg = kegg.drop_duplicates(subset='gi')
     print('Reading SEED mapping file...')
-    seed = pd.read_csv(args.seedmap, sep='\t')
+    seed = pd.read_csv(args.seedmap, sep='\t', header=None)
     seed.columns = ['gi', 'seed']
-    print('Reading eggNOG mapping file...')
-    eggnog = pd.read_csv(args.eggNOGmap, sep='\t')
-    eggnog.columns = ['gi', 'eggnog']
-    print('Reading CARD mapping file...')
-    card = pd.read_csv(args.cardmap, sep='\t')
-    card.columns = ['gi', 'card']
-    print('Reading InterPro mapping file...')
-    interpro = pd.read_csv(args.interpromap, sep='\t')
-    interpro.columns = ['gi', 'interpro']
+    seed = seed.drop_duplicates(subset='gi')
+    # print('Reading eggNOG mapping file...')
+    # eggnog = pd.read_csv(args.eggNOGmap, sep='\t', header=None)
+    # eggnog.columns = ['gi', 'eggnog']
+    # eggnog = eggnog.drop_duplicates(subset='gi')
+    # print('Reading CARD mapping file...')
+    # card = pd.read_csv(args.cardmap, sep='\t', header=None)
+    # card.columns = ['gi', 'card']
+    # card = card.drop_duplicates(subset='gi')
+    # print('Reading InterPro mapping file...')
+    # interpro = pd.read_csv(args.interpromap, sep='\t', header=None)
+    # interpro.columns = ['gi', 'interpro']
+    # interpro = interpro.drop_duplicates(subset='gi')
 
     keggseed = pd.merge(kegg, seed, how='inner', on='gi')
-    keggnog = pd.merge(kegg, eggnog, how='inner', on='gi')
-    keggcard = pd.merge(kegg, card, how='inner', on='gi')
-    kegginterpro = pd.merge(kegg, interpro, how='inner', on='gi')
-    seedeggnog = pd.merge(seed, eggnog, how='inner', on='gi')
-    seedcard = pd.merge(seed, card, how='inner', on='gi')
-    seedinterpro = pd.merge(seed, interpro, how='inner', on='gi')
-    eggnogcard = pd.merge(eggnog, card, how='inner', on='gi')
-    eggnoginterpro = pd.merge(eggnog, interpro, how='inner', on='gi')
-    cardinterpro = pd.merge(card, interpro, how='inner', on='gi')
+    # keggnog = pd.merge(kegg, eggnog, how='inner', on='gi')
+    # keggcard = pd.merge(kegg, card, how='inner', on='gi')
+    # kegginterpro = pd.merge(kegg, interpro, how='inner', on='gi')
+    # seedeggnog = pd.merge(seed, eggnog, how='inner', on='gi')
+    # seedcard = pd.merge(seed, card, how='inner', on='gi')
+    # seedinterpro = pd.merge(seed, interpro, how='inner', on='gi')
+    # eggnogcard = pd.merge(eggnog, card, how='inner', on='gi')
+    # eggnoginterpro = pd.merge(eggnog, interpro, how='inner', on='gi')
+    # cardinterpro = pd.merge(card, interpro, how='inner', on='gi')
 
