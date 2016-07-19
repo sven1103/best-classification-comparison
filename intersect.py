bdd9cd0eaef5a241d303a0e8ae4dbcddc90fe9bd
Candice Schumann <candiceschumann@Candices-MacBook-Pro.local>
2016-07-19 18:11:42 +0200

    Added venn diagrams of three combinations

--- intersect.py
+++ intersect.py
@@ -1,6 +1,7 @@
 import argparse
 import pandas as pd
 from matplotlib_venn import venn2
+from matplotlib_venn import venn3
 from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
@@ -38,12 +39,12 @@ if __name__ == "__main__":
 
     intersects = [[0 for x in range(len(names))] for y in range(len(names))]
     count = 0
-    indexes = [[-1 for x in range(len(names))] for y in range(len(names))]
+    indexes3 = [[-1 for x in range(len(names))] for y in range(len(names))]
     inters = []
     candice = [[0 for x in range(len(names))]for y in range(len(names))]
     for i in range(0,len(names)-1):
         for j in range(i+1,len(names)):
-            print("Running merge on %s with %s" % (names[i], names[j]))
+            print("Running merge on %s with %s..." % (names[i], names[j]))
             inter = pd.merge(outer_sets[i],outer_sets[j])
             inters.append(inter)
             indexes[i][j] = count
@@ -59,6 +60,7 @@ if __name__ == "__main__":
             print(str.format("{0} to {1}: {2}", names[j], names[i], candice[j][i]))
 
 
+    print("Creating venn diagrams...")
     for i in range(0,len(names)-1):
         for j in range(i+1,len(names)):
             jac = intersects[i][j]/(len(outer_sets[i])+len(outer_sets[j])-intersects[i][j]+0.0)
@@ -88,12 +90,78 @@ if __name__ == "__main__":
             plt.savefig("venn_%s_%s"%(names[i],names[j]))
             plt.clf()
         # plt.show()
+    print("Creating alternate jaccard bar charts...")
+    for i in range(0,len(names)):
+        plt.bar(range(1,len(names)),candice[i][0:i]+candice[i][(i+1):len(names)],align="center",color='orange',alpha=0.5)
+        plt.title("%s's alternate Jaccard scores"%names[i])
+        plt.xticks(range(1,len(names)),names[0:i]+names[(i+1):len(names)])
+        plt.ylim([0,1])
+        plt.savefig("alt_jaccard/alt_jaccard_%s"%names[i])
+        plt.clf()
+            # plt.show()
 
-for i in range(0,len(names)):
-    plt.bar(range(1,len(names)),candice[i][0:i]+candice[i][(i+1):len(names)],align="center",color='orange',alpha=0.5)
-    plt.title("%s's alternate Jaccard scores against all other methods"%names[i])
-    plt.xticks(range(1,len(names)),names[0:i]+names[(i+1):len(names)])
-    plt.show()
+    inters3 = []
+    indexes3 = [[[-1 for x in range(len(names))] for y in range(len(names))] for z in range(len(names))]
+    count = 0
+    for i in range(0,len(names)-2):
+        for j in range(i+1,len(names)-1):
+            for k in range(j+1,len(names)):
+                print("merging %s %s %s..." % (names[i], names[j], names[k]))
+                inters3.append(pd.merge(inters[indexes[i][j]],outer_sets[k]))
+                indexes3[i][j][k] = count
+                indexes3[j][i][k] = count
+                print("size: {0:,}".format(len(inters3[count])))
+                count += 1
+
+    for i in range(0,len(names)-2):
+        for j in range(i+1,len(names)-1):
+            for k in range(j+1,len(names)):
+                sb = (len(outer_sets[i])-intersects[i][j]-intersects[i][k]+len(inters3[indexes3[i][j][k]]),
+                    len(outer_sets[j])-intersects[i][j]-intersects[j][k]+len(inters3[indexes3[i][j][k]]),
+                    intersects[i][j]-len(inters3[indexes3[i][j][k]]),
+                    len(outer_sets[k])-intersects[i][k]-intersects[j][k]+len(inters3[indexes3[i][j][k]]),
+                    intersects[i][k]-len(inters3[indexes3[i][j][k]]),
+                    intersects[j][k]-len(inters3[indexes3[i][j][k]]),
+                    len(inters3[indexes3[i][j][k]]))
+                v = venn3(subsets=sb,set_labels=(names[i],names[j],names[k]))
+                v.get_label_by_id('100').set_text('')
+                v.get_label_by_id('010').set_text('')
+                v.get_label_by_id('110').set_text('')
+                v.get_label_by_id('001').set_text('')
+                v.get_label_by_id('101').set_text('')
+                v.get_label_by_id('011').set_text('')
+                v.get_label_by_id('111').set_text('')
+                v.get_label_by_id('100').set_size(10)
+                v.get_label_by_id('010').set_size(10)
+                v.get_label_by_id('110').set_size(10)
+                v.get_label_by_id('001').set_size(10)
+                v.get_label_by_id('101').set_size(10)
+                v.get_label_by_id('011').set_size(10)
+                v.get_label_by_id('111').set_size(10)
+                plt.annotate('{0:,}'.format(sb[0]), xy = v.get_label_by_id('100').get_position(), xytext = (-40,20), size = 'medium',
+                ha = 'center', textcoords = 'offset points', 
+                arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3, rad = 0.5', color = 'gray'))
+                plt.annotate('{0:,}'.format(sb[1]), xy = v.get_label_by_id('010').get_position(), xytext = (50,-20), size = 'medium',
+                ha = 'center', textcoords = 'offset points', 
+                arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3, rad = 0.5', color = 'gray'))
+                plt.annotate('{0:,}'.format(sb[2]), xy = v.get_label_by_id('110').get_position(), xytext = (-30,70), size = 'medium',
+                ha = 'center', textcoords = 'offset points', 
+                arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3, rad = 0.5', color = 'gray'))
+                plt.annotate('{0:,}'.format(sb[3]), xy = v.get_label_by_id('001').get_position(), xytext = (40,-50), size = 'medium',
+                ha = 'center', textcoords = 'offset points', 
+                arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3, rad = 0.5', color = 'gray'))
+                plt.annotate('{0:,}'.format(sb[4]), xy = v.get_label_by_id('101').get_position(), xytext = (-70,10), size = 'medium',
+                ha = 'center', textcoords = 'offset points', 
+                arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3, rad = 0.5', color = 'gray'))
+                plt.annotate('{0:,}'.format(sb[5]), xy = v.get_label_by_id('011').get_position(), xytext = (70,-20), size = 'medium',
+                ha = 'center', textcoords = 'offset points',
+                arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3, rad = 0.5', color = 'gray'))
+                plt.annotate('{0:,}'.format(sb[6]), xy = v.get_label_by_id('111').get_position(), xytext = (-50,20), size = 'medium',
+                ha = 'center', textcoords = 'offset points',
+                arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3, rad = 0.5', color = 'gray'))
+                plt.savefig("venn/venn_%s_%s_%s" % (names[i],names[j],names[k]))
+                plt.clf()
+                # plt.show()
 
             
     # keggseed = pd.merge(kegg, seed, how='inner', on='gi')
